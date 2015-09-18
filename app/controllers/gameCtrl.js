c361e6df14680dd7f5196403aa89725337e47775
Ryan Tanay <ryantanay@newevolution.org>
2015-09-18 16:17:29 -0500

    Mousetracking is now working as intended, added option for keyboard controls as well, still needs user option integration, pickup & dropoff mechanics are go

--- app/controllers/gameCtrl.js
+++ app/controllers/gameCtrl.js
@@ -60,41 +60,41 @@ define([
     var aminoGroup;
     var countHolder;
     var carriedAmino;
-    var aminoToCollect;
+    var mouse = false;
     var aminosRemaining;
     var blinkCounter = 0;
     var homeSafe = false;
     var aminoToCollectGroup;
+    var spinningOut = false;
+    var carryingAmino = false;
     var proteinAminos = ["A", "R", "N", "D", "C", "Q", "H", "I", "L", "T"];
 
     function theGame() {
-      game.state.add("theGame", {preload: preload, create: create, update: update, render: render});
+      game.state.add("theGame", {preload: preload, create: create, update: update});
       game.state.start("theGame");
 
+//-------------------------------------------------------------------------------------------------      
+
       function create() {
-        aminosRemaining = proteinAminos.length;
-
-        game.add.tileSprite(0, 0, 1200, 1200, "background");
-
-        game.world.setBounds(0, 0, 1200, 1200);
-
+        // Generic setup ##########################################################################
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.add.tileSprite(0, 0, 1200, 1200, "background");
+        game.world.setBounds(0, 0, 1200, 1200);
+        if(!mouse) {
+          cursors = game.input.keyboard.createCursorKeys();
+        }
 
+        // Player block ###########################################################################
         player = game.add.sprite(game.world.centerX, game.world.centerY, "player");
         player.anchor.setTo(0.5, 0.5); //so it flips around its middle
-
         game.physics.arcade.enable(player);
-        player.body.allowRotation = false;
-
         player.body.collideWorldBounds = true;
+        game.camera.follow(player);
 
+        // Amino swarm block ######################################################################
         aminoGroup = game.add.group();
-
         aminoGroup.enableBody = true;
-
-        aminoToCollectGroup = game.add.group();
-
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 15; i++) {
           var theAmino = "";
           if(i < 5) {
             theAmino = proteinAminos[i];
@@ -108,16 +108,21 @@ define([
             aminoY = game.rnd.integerInRange(0, 1200);
           }
           var anAmino = aminoGroup.create(aminoX, aminoY, theAmino);
-          anAmino.anchor.setTo(0.5, 0.5); //so it flips around its middle
-          anAmino.rotation = game.rnd.realInRange(-0.2, 0.2);
           anAmino.body.velocity.set(game.rnd.integerInRange(-100, 100), game.rnd.integerInRange(-100, 100));
-          anAmino.body.collideWorldBounds = true;
+          anAmino.rotation = game.rnd.realInRange(-0.2, 0.2);
           anAmino.body.bounce.y = 0.6 + Math.random() * 0.35;
           anAmino.body.bounce.x = 0.6 + Math.random() * 0.35;
+          anAmino.body.collideWorldBounds = true;
+          anAmino.anchor.setTo(0.5, 0.5);
         }
 
-        game.camera.follow(player);
+        // Collection instructions block ##########################################################
+        aminoToCollectGroup = game.add.group();
+        var aminoToCollect = aminoToCollectGroup.create(0, 0, proteinAminos[0]);
+        aminoToCollect.fixedToCamera = true;
 
+        // Aminos to collect count ################################################################
+        aminosRemaining = proteinAminos.length;
         countHolder = game.add.sprite(0,0);
         countHolder.fixedToCamera = true;
         countText = game.add.text(0,0,aminosRemaining + " Left!");
@@ -125,44 +130,36 @@ define([
         countHolder.cameraOffset.x = 140;
         countHolder.cameraOffset.y = 10;
 
-        for (var k = 0; k < proteinAminos.length; k++) {
-          var aminoToCollect = aminoToCollectGroup.create(10 + (70 * k), 10, proteinAminos[k]);
-          aminoToCollect.fixedToCamera = true;
-          if(k > 1) {
-              aminoToCollectGroup.children[k].alpha = 0;
-          }
-          if(k === 1) {
-            aminoToCollectGroup.children[k].scale.x = 0.7;
-            aminoToCollectGroup.children[k].scale.y = 0.7;
-          }
-        }
 
-        ribosome = game.add.sprite(0,0, "ribosome");
+        // Ribosome block #########################################################################
+        ribosome = game.add.sprite(0, 0, "ribosome");
         game.physics.arcade.enable(ribosome);
+        eyes = game.add.sprite(0,0, "eyes");
         ribosome.fixedToCamera = true;
         ribosome.cameraOffset.x = 20;
         ribosome.cameraOffset.y = 410;
-        
-        eyes = game.add.sprite(0,0, "eyes");
         eyes.fixedToCamera = true;
         eyes.cameraOffset.x = 85;
         eyes.cameraOffset.y = 432;
 
+        // Gameover / win text ####################################################################
         spriteText = game.add.sprite(0, 0);
         spriteText.fixedToCamera = true;
         spriteText.visible = false;
       }
 
+//-------------------------------------------------------------------------------------------------
+
       function update() {
         game.physics.arcade.collide(aminoGroup, aminoGroup, rotateBoth, null, this);
-        game.physics.arcade.overlap(player, ribosome, homeBase, null, this);
-        // game.physics.arcade.overlap(player, aminoGroup, checkAmino, null, this);
+        game.physics.arcade.overlap(player, ribosome, inTheRibosome, null, this);
+        game.physics.arcade.collide(player, aminoGroup, checkAmino, null, this);
 
+        // Ribosome blinking ######################################################################
         var blink = game.rnd.integerInRange(0, 300);
         if(blink === 10 && eyes.frame === 0) {
           eyes.frame = 1;
         }
-
         if(eyes.frame === 1) {
           if(blinkCounter < 5) {
             blinkCounter++;
@@ -172,42 +169,115 @@ define([
           }
         }
 
+        // Player Motion ##########################################################################
         player.frame = 0;
+        if(spinningOut) {
+          player.rotation += 0.5;
+        } else {
+          if(mouse) { // Mouse-follow controls
+            game.physics.arcade.moveToPointer(player, 60, game.input.activePointer, 400);
+          } else { // Keyboard controls
+            player.body.velocity.x = 0;
+            player.body.velocity.y = 0;
+            if (cursors.left.isDown) {
+              player.body.velocity.x = -300;
+              player.frame = 1;
+            } else if (cursors.right.isDown) {
+              player.body.velocity.x = 300;
+              player.frame = 1;
+            }
+            if (cursors.up.isDown) {
+              player.body.velocity.y = -300;
+            } else if (cursors.down.isDown) {
+              player.body.velocity.y = 300;
+            }
+          }
 
-        // game.physics.arcade.moveToPointer(player, 300);
+          if(Math.abs(player.body.velocity.x) > 150) {
+            player.frame = 1;
+          }
 
-        player.rotation = game.physics.arcade.moveToPointer(player, 50, game.input.activePointer, 500);
+          if(player.body.velocity.y < -150) {
+            player.frame = 2;
+          } else if(player.body.velocity.y > 150) {
+            player.frame = 3;
+          }
 
-        // if(((Math.abs((player.body.x + player.body.width/2) - game.input.x)) < 5) && ((Math.abs((player.body.y + player.body.height/2) - game.input.y)) < 5))
-        // {
-        //     player.body.velocity.setTo(0, 0);
-        // }
-
-        if(Math.abs(player.body.velocity.x) > 50) {
-          player.frame = 1;
+          if(player.body.velocity.x > 0) {
+            player.scale.x = -1;
+            if(player.children[0]) {
+              player.children[0].frame = 1;
+              player.children[0].scale.x = -1;
+            }
+          } else if(player.body.velocity.x < 0) {
+            player.scale.x = 1;
+            if(player.children[0]) {
+              player.children[0].frame = 0;
+              player.children[0].scale.x = 1;
+            }
+          }
         }
 
-        // if(player.body.velocity.y < -150) {
-        //   player.frame = 2;
-        // } else if(player.body.velocity.y > 150) {
-        //   player.frame = 3;
-        // }
+        // Aminos on stage management #############################################################
+        var aliveCount = 0;
+        for (var l = 0; l < aminoGroup.children.length; l++) {
+          if(aminoGroup.children[l].alive === true) {
+            aliveCount++;
+          }
+          // If an amino is going to slow, speed it up
+          if(Math.abs(aminoGroup.children[l].body.velocity.x) < 40 || Math.abs(aminoGroup.children[l].body.velocity.y) < 40) {
+            aminoGroup.children[l].body.velocity.set(game.rnd.integerInRange(-100, 100), game.rnd.integerInRange(-100, 100));
+          }
+          // Make sure the amino is facing the diretion it's traveling
+          if(aminoGroup.children[l].body.velocity.x > 0) {
+            aminoGroup.children[l].frame = 1;
+          } else if(aminoGroup.children[l].body.velocity.x < 0) { 
+            aminoGroup.children[l].frame = 0;
+          }
+        }
+        // Always keep 15 aminos on screen
+        if(aliveCount < 15) {
+          var theAmino = proteinAminos[0];
+          console.log("theAmino", theAmino);
+          if(theAmino !== undefined) { // if the array is empty i.e. the level is complete
+            var anAminoX = 201;
+            var anAminoY = 301;
+            while((anAminoX > player.x - 400 && anAminoX < player.x + 400) && (anAminoY > player.y - 400 && anAminoY < player.y + 400)) {
+              anAminoX = game.rnd.integerInRange(0, 1200);
+              anAminoY = game.rnd.integerInRange(0, 1200);
+            }
+            var anAmino = aminoGroup.create(anAminoX, anAminoY, theAmino);
+            anAmino.body.velocity.set(game.rnd.integerInRange(-100, 100), game.rnd.integerInRange(-100, 100));
+            anAmino.rotation = game.rnd.realInRange(-0.2, 0.2);
+            anAmino.body.bounce.y = 0.6 + Math.random() * 0.35;
+            anAmino.body.bounce.x = 0.6 + Math.random() * 0.35;
+            anAmino.body.collideWorldBounds = true;
+            anAmino.anchor.setTo(0.5, 0.5);
+          }
+        }
 
-        // if(player.body.velocity.x > 0) {
-        //   player.scale.y = 1;
-        // } else if(player.body.velocity.x < 0) {
-        //   player.scale.y = -1;
-        // }
-
-        function homeBase() {
+        // Called functions #######################################################################
+        function inTheRibosome() {
           if(!homeSafe) {
             homeSafe = true;
             console.log("Ding!");
           }
+          if(carriedAmino !== undefined && carryingAmino === true) {
+            carryingAmino = false;
+            if(proteinAminos.length === 1) {
+              // won goes here
+            }
+            proteinAminos.splice(0, 1);
+            carriedAmino.destroy();
+            countText.text = proteinAminos.length + " Left!";
+            aminoToCollectGroup.removeAll();
+            var aminoToCollect = aminoToCollectGroup.create(0, 0, proteinAminos[0]);
+            aminoToCollect.fixedToCamera = true;
+          }
         }
 
         function checkAmino (player, anAmino) {
-          if(proteinAminos[0] === anAmino.key) { // left-to-right aminoToCollect
+          if(proteinAminos[0] === anAmino.key && carryingAmino === false) {
             goodAmino(player, anAmino);
           } else {
             badAmino(player, anAmino);
@@ -220,80 +290,29 @@ define([
         }
 
         function goodAmino (player, anAmino) {
-          if(proteinAminos.length === 1) {
-            stateText = game.add.text(300, 400, " YOU WON! \n Click to restart");
-            spriteText.addChild(stateText);
-            spriteText.visible = true;
-            game.input.onTap.addOnce(create,this);
-          }
-          proteinAminos.splice(0, 1); // left-to-right aminoToCollect
-          anAmino.kill();
-          countText.text = proteinAminos.length + " Left!";
-          aminoToCollectGroup.removeAll(); // left-to-right aminoToCollect
-          for (var m = 0; m < proteinAminos.length; m++) {
-            var aminoToCollect = aminoToCollectGroup.create(10 + (70 * m), 10, proteinAminos[m]);
-            aminoToCollect.fixedToCamera = true;
-            if(m > 1) {
-              aminoToCollectGroup.children[m].alpha = 0;
-            }
-            if(m === 1) {
-              aminoToCollectGroup.children[m].scale.x = 0.7;
-              aminoToCollectGroup.children[m].scale.y = 0.7;
-            }
+          if(!spinningOut && !carryingAmino) {
+            carryingAmino = true;
+            anAmino.kill();
+            carriedAmino = game.add.sprite(0, -60, anAmino.key);
+            carriedAmino.anchor.setTo(0.5, 0.5);
+            player.addChild(carriedAmino);
           }
         }
 
         function badAmino (player, anAmino) {
-          player.rotation++;
-          player.scale.x -= 0.1;
-          player.scale.y -= 0.1;
-          player.alpha -= 0.1;
-          setTimeout(function() {
-            player.kill();
-          }, (200));
-          stateText = game.add.text(300, 400, " GAME OVER \n Click to restart");
-          spriteText.addChild(stateText);
-          spriteText.visible = true;
-          game.input.onTap.addOnce(create, this);
-        }
-
-        var aliveCount = 0;
-        for (var l = 0; l < aminoGroup.children.length; l++) {
-          if(aminoGroup.children[l].alive === true) {
-            aliveCount++;
-          }
-          if(Math.abs(aminoGroup.children[l].body.velocity.x) < 40 || Math.abs(aminoGroup.children[l].body.velocity.y) < 40) {
-            aminoGroup.children[l].body.velocity.set(game.rnd.integerInRange(-100, 100), game.rnd.integerInRange(-100, 100));
-          }
-          if(aminoGroup.children[l].body.velocity.x > 0) {
-            aminoGroup.children[l].frame = 1;
-          } else if(aminoGroup.children[l].body.velocity.x < 0) { 
-            aminoGroup.children[l].frame = 0;
-          }
-        }
-
-        // Always keep 10 aminos on screen
-        if(aliveCount < 10) {
-          var theAmino = proteinAminos[0];
-          var anAminoX = 201;
-          var anAminoY = 301;
-          while((anAminoX > player.x - 400 && anAminoX < player.x + 400) && (anAminoY > player.y - 400 && anAminoY < player.y + 400)) {
-            anAminoX = game.rnd.integerInRange(0, 1200);
-            anAminoY = game.rnd.integerInRange(0, 1200);
-          }
-          var anAmino = aminoGroup.create(anAminoX, anAminoY, theAmino);
-          anAmino.anchor.setTo(0.5, 0.5); //so it flips around its middle
+          spinningOut = true;
           anAmino.rotation = game.rnd.realInRange(-0.2, 0.2);
-
-          anAmino.body.velocity.set(game.rnd.integerInRange(-100, 100), game.rnd.integerInRange(-100, 100));
-
-          anAmino.body.collideWorldBounds = true;
-
-          anAmino.body.bounce.y = 0.6 + Math.random() * 0.35;
-          anAmino.body.bounce.x = 0.6 + Math.random() * 0.35;
+          player.body.velocity.x = 0;
+          player.body.velocity.y = 0;
+          setTimeout(function() {
+            spinningOut = false;
+            player.rotation = 0;
+          }, (1000));
         }
 
       } 
+
+//-------------------------------------------------------------------------------------------------
 
       function render() {
         game.debug.body(player);
