b7d51f416270d980427fa91c521a1da4ef4edb09
Ryan Tanay <ryantanay@newevolution.org>
2016-06-08 13:11:48 -0500

    Added Firebase app info to stub in fireconf

--- app/factories/fireconf.js
+++ app/factories/fireconf.js
@@ -6,10 +6,10 @@ define([
   .module("AminoApp.fireconf", [])
   .factory("fireconf", function() {
     firebase.initializeApp({
-      apiKey: "",
-      authDomain: "",
-      databaseURL: "",
-      storageBucket: ""
+      apiKey: "AIzaSyAs7q2YBQCw0TDW0P-DF5DgAXyyV4YqmOI",
+      authDomain: "proteinpanic.firebaseapp.com",
+      databaseURL: "https://proteinpanic.firebaseio.com",
+      storageBucket: "project-1082984889749344373.appspot.com",
     });
     return firebase;
   });
